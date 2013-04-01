9689133ba2e379b6e53ddcd2425e5bac81500a5f
David Ellis <d.f.ellis@ieee.org>
2013-04-01 16:45:47 -0700

    Fixing bugs in the argsArray slicing and the isAsync checks, while adding more tests. Over 50% code coverage. :)

--- test/test.js
+++ test/test.js
@@ -22,3 +22,52 @@ exports.simpleEvent = function(test) {
     });
     emitter.emit('hi');
 };
+
+exports.cancelableEvent = function(test) {
+    bootstrap(test);
+    test.expect(2);
+    var emitter = new EventEmitter();
+    emitter.on('toCancel', function() {
+        test.ok(true, 'received the event');
+        return false;
+    });
+    emitter.emit('toCancel', function(result) {
+        test.ok(result === false, 'informed to cancel the event');
+        test.done();
+    });
+};
+
+exports.asyncEventListener = function(test) {
+    bootstrap(test);
+    test.expect(3);
+    var emitter = new EventEmitter();
+    emitter.on('toAccept', function(callback) {
+        test.ok(true, 'received the event');
+        test.ok(callback instanceof Function, 'received a callback function');
+        callback(true);
+    });
+    emitter.emit('toAccept', function(result) {
+        test.ok(result, 'informed to continue the event');
+        test.done();
+    });
+};
+
+exports.forceListenerType = function(test) {
+    bootstrap(test);
+    test.expect(3);
+    var emitter = new EventEmitter();
+    emitter.onSync('toAccept', function(optionalValue) {
+        test.ok(true, 'received the event');
+        test.ok(!optionalValue, 'nothing passed into the optionalValue slot');
+        // not returning false is the same as returning true
+    });
+    emitter.onAsync('toAccept', function(optionalValue, callback) {
+        if(!callback) callback = optionalValue;
+        test.ok(true, 'received the event');
+        callback();
+    });
+    emitter.emit('toAccept', function(result) {
+        test.ok(result, 'informed to continue the event');
+        test.done();
+    });
+};
