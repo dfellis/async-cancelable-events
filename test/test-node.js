d33b1b3d9a45d1b0febd844c2a1f10c666fd61d9
David Ellis <d.f.ellis@ieee.org>
2013-06-05 10:27:27 -0700

    Adding coveralls support.

--- test/test-node.js
+++ test/test-node.js
@@ -1,5 +1,6 @@
 var jscoverage = require('jscoverage');
 jscoverage.enableCoverage(true);
+var coveralls = require('coveralls');
 var EventEmitter = jscoverage.require(module, '../lib/async-cancelable-events');
 var tests = require('./test');
 tests.getEventEmitter(EventEmitter);
@@ -20,13 +21,16 @@ exports.jscoverage = function(test) {
     if (typeof _$jscoverage === 'undefined') {
         return;
     }
+    var lcov = "";
     for (var i in _$jscoverage) {
         file = i;
+        lcov += "SF:" + file + "\n";
         tmp = _$jscoverage[i];
         if (typeof tmp === 'function' || tmp.length === undefined) continue;
         total = touched = 0;
         for (n = 0, len = tmp.length; n < len; n++) {
             if (tmp[n] !== undefined) {
+                lcov += "DA:" + n + "," + tmp[n] + "\n";
                 total ++;
                 if (tmp[n] > 0)
                     touched ++;
@@ -34,5 +38,7 @@ exports.jscoverage = function(test) {
         }
         test.equal(total, touched, 'All lines of code exercised by the tests');
     }
+    lcov += "end_of_record\n";
+    if(process.env.TRAVIS) coveralls.handleInput(lcov);
     test.done();
 };
